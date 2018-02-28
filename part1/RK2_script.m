948d6c112691b21134f71dd2c7b336e7083b0fcc
wmvndrscht <wv116@ic.ac.uk>
2018-02-28 21:20:55 +0000

    Clearer naming and decrease h for clearer graphs

--- part1/RK2_script.m
+++ part1/RK2_script.m
@@ -14,7 +14,9 @@ R = 1000;
 C = 100e-9;
 q0 = 500e-9;
 tf = 0.003;
-h  = 0.00001;
+h  = 0.000001;
+
+method = "midpoint";
 
 % our function, of (t,q) aka(x,y)
 func = @(t,q) (1/R)*(Vin(t) - q/C );
@@ -25,12 +27,12 @@ func = @(t,q) (1/R)*(Vin(t) - q/C );
 Vin = @(t) 2.5*heaviside(t);
 
 % Solving for q
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Step_Vout, Step_t] = RK2(q0,h,tf,func,Vin,R,C,method);
 % In our function case we are investigating Vout, where Vout = q/C
 
 % % plotting the graph of the result
 subplot(1,2,1);
-plot(t,Vout);
+plot(Step_t,Step_Vout);
 title('Vout against time for Step Signal, input of 2.5V');
 xlabel('Time / s');
 ylabel('Vout / V');
@@ -42,14 +44,14 @@ ylabel('Vout / V');
 % where tau = 100 (microseconds^2) and tau = 100 microseconds respectively
 
 Vin = @(t) 2.5*exp(-(t^2)/(100e-12));
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Impulse_Vout, Impulse_t] = RK2(q0,h,tf,func,Vin,R,C,method);
 subplot(1,2,2);
 hold on
-plot(t,Vout);
+plot(Impulse_t,Impulse_Vout);
  
 Vin = @(t) 2.5*exp(-t/(100e-6));
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'red');
+[Impulse_Vout2, Impulse_t2] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Impulse_t2,Impulse_Vout2,'red');
 
 
 %% Sine, Square and Sawtooth waves
@@ -63,26 +65,25 @@ plot(t,Vout,'red');
 
 
 Vin = @(t) 5*sin((2*pi/(10e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Sine_Vout1, Sine_t1] = RK2(q0,h,tf,func,Vin,R,C,method);
 figure(2)
 hold on
-plot(t,Vout,'m');
+plot(Sine_t1,Sine_Vout1,'m');
 
 Vin = @(t) 5*sin((2*pi/(100e-6))*t);
-func = @(t,q) (1/R)*(Vin(t) - q/C );
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Sine_Vout2, Sine_t2] = RK2(q0,h,tf,func,Vin,R,C,method);
 hold on
-plot(t,Vout,'r');
+plot(Sine_t2,Sine_Vout2,'r');
 
 Vin = @(t) 5*sin((2*pi/(500e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Sine_Vout3, Sine_t3] = RK2(q0,h,tf,func,Vin,R,C,method);
 hold on
-plot(t,Vout,'b');
+plot(Sine_t3,Sine_Vout3,'b');
 
 Vin = @(t) 5*sin((2*pi/(1000e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Sine_Vout4, Sine_t4] = RK2(q0,h,tf,func,Vin,R,C,method);
 hold on
-plot(t,Vout,'g');
+plot(Sine_t4,Sine_Vout4,'g');
 
 title('Vout against time for Sine Signal, input of 5V');
 xlabel('Time / s');
@@ -93,22 +94,22 @@ legend('T=10e-6','T=100e-6', 'T=500e-6','T=1000e-6');
 %% Square
 
 Vin = @(t) 5*square((2*pi/(10e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Square_Vout1, Square_t1] = RK2(q0,h,tf,func,Vin,R,C,method);
 figure(3)
 hold on
-plot(t,Vout,'m');
+plot(Square_t1,Square_Vout1,'m');
 
 Vin = @(t) 5*square((2*pi/(100e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'r');
+[Square_Vout2, Square_t2] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Square_t2,Square_Vout2,'r');
 
 Vin = @(t) 5*square((2*pi/(500e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'b');
+[Square_Vout3, Square_t3] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Square_t3,Square_Vout3,'b');
 
 Vin = @(t) 5*square((2*pi/(1000e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'g');
+[Square_Vout4, Square_t4] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Square_t4,Square_Vout4,'g');
 
 title('Vout against time for Square Signal, input of 5V');
 xlabel('Time / s');
@@ -118,22 +119,22 @@ legend('T=10e-6','T=100e-6', 'T=500e-6','T=1000e-6');
 %% Sawtooth
 
 Vin = @(t) 5*sawtooth((2*pi/(10e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
+[Sawtooth_Vout1, Sawtooth_t1] = RK2(q0,h,tf,func,Vin,R,C,method);
 figure(5);
 hold on;
-plot(t,Vout,'m');
+plot(Sawtooth_t1,Sawtooth_Vout1,'m');
 
 Vin = @(t) 5*sawtooth((2*pi/(100e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'r');
+[Sawtooth_Vout2, Sawtooth_t2] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Sawtooth_t2,Sawtooth_Vout2,'r');
 
 Vin = @(t) 5*sawtooth((2*pi/(500e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'b');
+[Sawtooth_Vout3, Sawtooth_t3] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Sawtooth_t3,Sawtooth_Vout3,'b');
 
 Vin = @(t) 5*sawtooth((2*pi/(1000e-6))*t);
-[Vout, t] = RK2(q0,h,tf,func,Vin,R,C);
-plot(t,Vout,'g');
+[Sawtooth_Vout4, Sawtooth_t4] = RK2(q0,h,tf,func,Vin,R,C,method);
+plot(Sawtooth_t4,Sawtooth_Vout4,'g');
 
 
 title('Vout against time for Sawtooth Signal, input of 5V');
