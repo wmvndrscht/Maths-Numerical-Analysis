7669b7875405cb0d1140c55bdd43ad14817e716b
LYZFU <zf816@ic.ac.uk>
2018-03-02 12:42:34 +0000

    comment

--- part3/relaxation1.m
+++ part3/relaxation1.m
@@ -1,3 +1,61 @@
+<<<<<<< HEAD
+clear;
+%total numbers
+x=100;
+y=100;
+h=1/x; %gridsize
+k=(0.6*x)/(4*x-4); %average of all values of known boundary points
+ep=0.15; %residual size
+
+%initially set all values to k
+for i=1:x+1
+    for j=1:y+1
+        u(i,j)=k;
+    end
+end
+
+%boundary conditions
+for i=1:y+1
+    u(1,i)=0;
+    u(x+1,i)=0;
+    u(i,x+1)=0;
+    u(i,1)=0;
+end
+
+for i=((0.2*x)+1):((0.8*x)+1)
+    u(i,1)=1;
+end
+
+
+cnt=x*y;
+for i=1:cnt
+    cnt=0;
+     for n = 2:x
+        for m = 2:y
+            r=(u(m+1, n)+u(m-1, n)+u(m, n+1)+u(m, n-1)-4*u(m, n))/4;
+            u(m,n) = u(m,n)+r;
+            if r<ep
+                cnt=cnt+1;
+            end
+        end
+     end
+end
+
+[X,Y]=meshgrid(0:h:1,0:h:1);
+S=surf(X,Y,u,u);
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 %implement boundary conditions
 
 % u(0; y) = u(1; y) = 0 ; for 0 < y < 1;
@@ -43,3 +101,4 @@ para = 1/(n-1);
 [X,Y] = meshgrid(0:para:1,0:para:1);
 S = surf(X,Y,unew,unew);
 
+>>>>>>> a1e98786c0223226090ccc32ae91b865de56c039
