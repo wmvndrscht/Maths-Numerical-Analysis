76496fa8ef7585d043b005ce6c6f86f0a7886825
wmvndrscht <wv116@ic.ac.uk>
2018-03-21 13:03:41 +0000

    updated so runs < 2 sec

--- part2/RLC.m
+++ part2/RLC.m
@@ -12,7 +12,7 @@ dqCi = 0;
 
 ti = 0; %start time = 0;
 tf = 0.2; %end time -> re adapt according to input
-h = 10^(-6); %small interval chosen
+h = 10^(-4); %small interval chosen
 
 %initialize empty arrays
 N = round(tf/h);
@@ -20,8 +20,8 @@ Vin = zeros(1,N);
 T = zeros(1,N);
 
 %input signals
-Vin1 = @(t) (5);
-Vin2 = @(t) (5*exp((-t^2)/(3e-6)));
+Vin1 = @(t) 5*heaviside(t);
+Vin2 = @(t) (5*exp((-t.^2)/(3e-6)));
 Vin3A = @(t) (5*square(2*pi*5*t));
 Vin3B = @(t) (5*square(2*pi*109*t));
 Vin3C = @(t) (5*square(2*pi*500*t));
@@ -29,26 +29,21 @@ Vin4A = @(t) (5*sin(2*pi*5*t));
 Vin4B = @(t) (5*sin(2*pi*109*t));
 Vin4C = @(t) (5*sin(2*pi*500*t));
 
-%Vin1
-for j = 1:N-1
-    Vin(j) = feval(Vin1, T(j)); %change Vin1 to test all inputs 
-    T(j+1) = T(j) + h;
-end
-
+tf = 0.05;
 f1 = @(t, qC, dqC) dqC;
-f2 =@(t, qC, dqC) (Vin1(t)-R*dqC-qC/C)/L; %change Vin1 here as well
+f2 = @(t, qC, dqC) (Vin1(t)-R*dqC-qC/C)/L; %change Vin1 here as well
 
 %run rk4th
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 
 %plot
-figure(1); clf(1)
+figure(1); %clf(1)
 dqC_out = dqC_out * R;
-plot(t_out, dqC_out, 'b')
+plot(t_out, dqC_out, 'b',t_out, Vin1(t_out), 'r')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
-xlabel('Time(s)')
+%plot(t_out, Vin1(t_out), 'r')
+xlabel('Time(s)');
 title('Input 1: 5');
 ylabel('Voltage(V)')
 set(gca, 'fontsize', 16);
@@ -57,19 +52,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin2
-for j = 1:N-1
-    Vin(j) = feval(Vin2, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.05;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin2(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(2);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin2(t_out), 'r');
 xlabel('Time(s)')
 title('Input 2: 5*exp((-t^2)/(3e-6))');
 ylabel('Voltage(V)')
@@ -79,19 +71,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin3A
-for j = 1:N-1
-    Vin(j) = feval(Vin3A, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.2;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin3A(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(3);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin3A(t_out), 'r');
 xlabel('Time(s)')
 title('Input 3A: square 5Hz');
 ylabel('Voltage(V)')
@@ -100,19 +89,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin3B
-for j = 1:N-1
-    Vin(j) = feval(Vin3B, T(j)); 
-    T(j+1) = T(j) + h;
-end
+tf = 0.04;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin3B(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(4);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin3B(t_out), 'r');
 xlabel('Time(s)')
 title('Input 3B: square 109Hz');
 ylabel('Voltage(V)')
@@ -122,19 +108,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin3C
-for j = 1:N-1
-    Vin(j) = feval(Vin3C, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.02;
 f1 = @(t, qC, dqC) dqC;
-f2 =@(t, qC, dqC) (Vin3C(t)-R*dqC-qC/C)/L; 
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+f2 =@(t, qC, dqC) (Vin3C(t)-R*dqC-qC/C)/L;
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(5);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin3C(t_out), 'r');
 xlabel('Time(s)')
 title('Input 3C: square 500Hz');
 ylabel('Voltage(V)')
@@ -144,19 +127,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin4A
-for j = 1:N-1
-    Vin(j) = feval(Vin4A, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.2;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4A(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(6);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin4A(t_out), 'r');
 xlabel('Time(s)')
 title('Input 4A: sine 5Hz');
 ylabel('Voltage(V)')
@@ -165,19 +145,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin4B
-for j = 1:N-1
-    Vin(j) = feval(Vin4B, T(j)); 
-    T(j+1) = T(j) + h;
-end
+tf = 0.04;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4B(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(7);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin4B(t_out), 'r');
 xlabel('Time(s)')
 title('Input 3B: sine 109Hz');
 ylabel('Voltage(V)')
@@ -187,19 +164,16 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %*************************************************************************
 
 %Vin4C
-for j = 1:N-1
-    Vin(j) = feval(Vin4C, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.04;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4C(t)-R*dqC-qC/C)/L; 
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(8);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
 hold on;
 grid on;
-plot(t_out, Vin, 'r');
+plot(t_out, Vin4C(t_out), 'r');
 xlabel('Time(s)')
 title('Input 4C: sine 500Hz');
 ylabel('Voltage(V)')
@@ -209,13 +183,10 @@ legend('Output Signal Capacitor(V)', 'Input Signal (V)', 'location', 'northeast'
 %**************************************************************************
 
 %Squarewaves : comparing
-for j = 1:N-1
-    Vin(j) = feval(Vin3A, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.04;
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin3A(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(9);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
@@ -223,23 +194,16 @@ hold on;
 grid on;
 
 
-for j = 1:N-1
-    Vin(j) = feval(Vin3B, T(j));
-    T(j+1) = T(j) + h;
-end
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin3B(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'g')
 
-for j = 1:N-1
-    Vin(j) = feval(Vin3C, T(j));
-    T(j+1) = T(j) + h;
-end
+
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin3C(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'r')
 
@@ -253,13 +217,11 @@ legend('f=5Hz', 'f=109Hz', 'f=500Hz', 'location', 'northeast');
 %*************************************************************************
 
 %Sinewaves : comparing
-for j = 1:N-1
-    Vin(j) = feval(Vin4A, T(j));
-    T(j+1) = T(j) + h;
-end
+tf = 0.04;
+
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4A(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 figure(10);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'b')
@@ -267,23 +229,17 @@ hold on;
 grid on;
 
 
-for j = 1:N-1
-    Vin(j) = feval(Vin4B, T(j));
-    T(j+1) = T(j) + h;
-end
+
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4B(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'g')
 
-for j = 1:N-1
-    Vin(j) = feval(Vin4C, T(j));
-    T(j+1) = T(j) + h;
-end
+
 f1 = @(t, qC, dqC) dqC;
 f2 =@(t, qC, dqC) (Vin4C(t)-R*dqC-qC/C)/L;
-[t_out, qC_out, dqC_out] = RK_4th(f1, f2, ti, qCi, dqCi, tf, h);
+[t_out, qC_out, dqC_out] = RK4(f1, f2, ti, qCi, dqCi, tf, h);
 dqC_out = dqC_out * R;
 plot(t_out, dqC_out, 'r')
 
