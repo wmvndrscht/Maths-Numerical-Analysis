d7e0688c2212afdf04a27a2c6502d708b221c472
wmvndrscht <wv116@ic.ac.uk>
2018-03-21 14:53:08 +0000

    Update error_script.m

--- part1/error_script.m
+++ part1/error_script.m
@@ -1,20 +1,11 @@
 clear all
 
-% Write a script called error script.m in which you carry out error analysis for the basic case
-% with R, C, qC(0) as above, and as input a cosine wave of period T = 100 µs and amplitude V¯
-% in = 5 V.
-% Obtain the exact solution of the ODE, and for each method of the three from Exercise 1, obtain a
-% numercial solution: again, switching methods by only commenting out lines of code appropriately.
-% Compare the numerical solution with the exact solution, obtaining the error as a function of t. Plot the
-% error function. Vary the time step h for a suitable number and range of values, and obtain a log-log
-% plot to show the order of the error. Be sure to explain and justify your empirical observation.
-
 % the initial conditions
 R = 1000;
 C = 100e-9;
 q0 = 500e-9;
 ti = 0;
-tf = 0.001;
+tf = 0.005;
 h  = 0.000001;
 T = 100e-6;
 w = 2*pi/T;
@@ -23,20 +14,24 @@ N = round((tf-ti)/h);
 Vin = @(t) 5*cos(2*pi*t/T);
 
 % First find the exact solution
-
 [exact,t] = exactresult(R,C,q0,ti,tf,h,T,w);
 
-
-% Create graph for each of the 3 methods
+%% 
+% Find the results for each method for a defined h
 % 1. Heun  -  2. Midpoint  -  3. Ralston
 % On each graph compare exact with Runge Kutta method
-% As spec defines one must be able to comment out code,
-% this is repeated 3 times
 
 func = @(t,q) (1/R)*(Vin(t) - q/C );
-[Heun_Vout, Heun_t] = RK2(q0,h,tf,func,Vin,R,C, "heun");
-[Midpoint_Vout, Midpoint_t] = RK2(q0,h,tf,func,Vin,R,C, "midpoint");
-[Ralston_Vout, Ralston_t] = RK2(q0,h,tf,func,Vin,R,C, "ralston");
+[Heun_qout, Heun_t] = RK2(q0,h,tf,func,"heun");
+Heun_Vout = Heun_qout./C;
+[Midpoint_qout, Midpoint_t] = RK2(q0,h,tf,func, "midpoint");
+Midpoint_Vout = Midpoint_qout./C;
+[Ralston_qout, Ralston_t] = RK2(q0,h,tf,func,"ralston");
+Ralston_Vout = Ralston_qout./C;
+
+
+% Find the absolute error (method - exact) for points calculated for 
+% each method
 
 for i=1:N
     Heun_error(i) = abs(Heun_Vout(i)-exact(i));
@@ -44,34 +39,49 @@ for i=1:N
     Ralston_error(i) = abs(Ralston_Vout(i)-exact(i));   
 end
 
+% Plot each of the absolute errors as a function of t
 figure(1)
 hold on
-plot(Heun_t,Heun_error,'m-x');
-plot(Midpoint_t,Midpoint_error,'r-x');
-plot(Ralston_t,Ralston_error,'b-x');
+plot(Heun_t,Heun_error,'LineWidth',1.4,'Color',[0.8 0.8 1]);
+plot(Midpoint_t,Midpoint_error,'LineWidth',1.2,'Color',[0.0 0.8 1.0]);
+plot(Ralston_t,Ralston_error,'LineWidth',1.2,'Color',[0.5 0.2 0.9]);
+title('Absolute error for each method');
+xlabel('Time / s');
+ylabel('Absolute Error / V');
+legend('Heun','Midpoint', 'Ralston');
+
+
+%% Find max absolute error for different h
 
 t=ti:h:tf-h;
-delta=0.000001:0.000004:0.00003;
+delta=0.000001:0.000004:0.00002;
 
-for j=1:8
+for j=1:5
     [exact,t] = exactresult(R,C,q0,ti,tf,delta(j),T,w);
-    [Heunh_Vout, Heun_t] = RK2(q0,delta(j),tf,func,Vin,R,C, "heun");
-    [Midpointh_Vout, Midpoint_t] = RK2(q0,delta(j),tf,func,Vin,R,C, "midpoint");
-    [Ralstonh_Vout, Ralston_t] = RK2(q0,delta(j),tf,func,Vin,R,C, "ralston");
+    [Heunh_qout, Heun_t] = RK2(q0,delta(j),tf,func, "heun");
+    Heunh_Vout = Heunh_qout./C;
+    [Midpointh_qout, Midpoint_t] = RK2(q0,delta(j),tf,func,"midpoint");
+    Midpointh_Vout = Midpointh_qout./C;
+    [Ralstonh_qout, Ralston_t] = RK2(q0,delta(j),tf,func,"ralston");
+    Ralstonh_Vout = Ralstonh_qout./C;
     Herr(j) = max(abs(Heunh_Vout-exact));
     Merr(j) = max(abs(Midpointh_Vout-exact));
     Rerr(j) = max(abs(Ralstonh_Vout-exact));
 end
 
+% plot results on a log log graph to show linear relationship
 figure(2)
 hold on
-loglog(delta,Herr)
-loglog(delta,Merr)
-loglog(delta,Rerr)
+plot(log(delta),log(Herr),'LineWidth',1.4,'Color',[0.8 0.8 1]);
+plot(log(delta),log(Merr),'LineWidth',1.2,'Color',[0.0 0.8 1.0]);
+plot(log(delta),log(Rerr),'LineWidth',1.2,'Color',[0.5 0.2 0.9]);
+title('LogLog plot for Max absolute error vs h');
+xlabel('log(H) / s');
+ylabel('log(Max absolute error) / V');
+legend('Heun','Midpoint', 'Ralston');
 
 
-
-
+% function to calculate exact value of cosine function
 
 function [exact,time] = exactresult(R,C,q0,ti,tf,h,T,w)
     exactfunc = @(t) (5*C*cos(w*t))/(1+(w*R*C)^2) + (5*sin(w*t)*w*R*C^2)/(1+(w*R*C)^2) + (q0 - (5*C)/(1+(w*R*C)^2))*exp(-t/(R*C)) ;
