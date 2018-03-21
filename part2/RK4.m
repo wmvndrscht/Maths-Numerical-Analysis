25ac96c34908d39fdebcf11a6c10b9e0dec85ba0
wmvndrscht <wv116@ic.ac.uk>
2018-03-21 13:04:13 +0000

    Update and rename RK_4th.m to RK4.m

---
+++ part2/RK4.m
@@ -0,0 +1,34 @@
+function[x, y, z] = RK_4th(f1, f2, xi, yi, zi, tf, h)
+
+    % y and z are equivalent to q and q'
+
+    %initialise parameters as empty N value arrays
+    N = round(tf/h);
+    x = zeros(1,N);
+    y = zeros(1,N);
+    z = zeros(1,N);
+
+    %value at t0
+    x(1) = xi;
+    y(1) = yi;
+    z(1) = zi;
+
+    %RG4th over interval
+    for i=1:N-1
+
+        k1 = feval(f1 ,x(i), y(i), z(i));
+        m1 = feval(f2, x(i), y(i), z(i));
+        k2 = feval(f1, x(i) + h/2, y(i) + h/2*k1, z(i) + h/2*m1);
+        m2 = feval(f2, x(i) + h/2, y(i) + h/2*k1, z(i) + h/2*m1);
+        k3 = feval(f1, x(i) + h/2, y(i) + h/2*k2, z(i) + h/2*m2);
+        m3 = feval(f2, x(i) + h/2, y(i) + h/2*k2, z(i) + h/2*m2);
+        k4 = feval(f1, x(i) + h, y(i) + h*k3, z(i)+ h*m3);
+        m4 = feval(f2, x(i) + h, y(i) + h*k3, z(i)+ h*m3);
+
+        %return
+        y(i+1)= y(i) + h/6 * (k1 + 2*k2 + 2*k3 + k4);
+        z(i+1)= z(i) + h/6 * (m1 + 2*m2 + 2*m3 + m4);
+        x(i+1) = x(i) + h;
+
+    end
+end
